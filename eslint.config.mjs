7e1359ed3653ccd91a168a946d1528fea17261b8
Fabien Giannesini <fabien.giannesini@gmail.com>
2024-11-30 17:34:09 +0100

    Clean, add prettier and eslint

---
+++ eslint.config.mjs
@@ -0,0 +1,23 @@
+import tsPlugin from '@typescript-eslint/eslint-plugin';
+import tsParser from '@typescript-eslint/parser';
+import prettierPlugin from 'eslint-plugin-prettier';
+import prettierConfig from 'eslint-config-prettier';
+
+export default [
+  {
+    files: ['**/*.ts', '**/*.tsx'],
+    ignores: ['node_modules', 'dist'],
+    languageOptions: {
+      parser: tsParser,
+      ecmaVersion: 2022,
+      sourceType: 'module',
+    },
+    plugins: {
+      '@typescript-eslint': tsPlugin,
+      prettier: prettierPlugin,
+    },
+    rules: {
+      ...prettierConfig.rules,
+    },
+  },
+];
---
+++ prettier.config.mjs
@@ -0,0 +1,10 @@
+export default {
+  semi: true, // Ajouter des points-virgules à la fin des lignes
+  singleQuote: true, // Utiliser des guillemets simples au lieu des doubles
+  trailingComma: 'all', // Ajouter une virgule finale dans les objets, tableaux, etc.
+  printWidth: 80, // Limite de longueur d'une ligne avant un retour à la ligne
+  tabWidth: 2, // Largeur d'une tabulation (nombre d'espaces)
+  useTabs: false, // Utiliser des espaces plutôt que des tabulations
+  bracketSpacing: true, // Ajouter des espaces entre les accolades et leur contenu
+  arrowParens: 'always', // Ajouter des parenthèses autour des paramètres uniques dans les fonctions fléchées
+};
