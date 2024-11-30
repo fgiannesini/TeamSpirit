567229229390157d42963ef3c686bb331d98841b
Fabien Giannesini <fabien.giannesini@gmail.com>
2024-11-30 18:05:14 +0100

    Add tests

--- src/counter.ts
+++ src/counter.ts
@@ -1,4 +1,4 @@
-export function setupCounter(element: HTMLButtonElement) {
+export const setupCounter = (element: HTMLButtonElement) => {
   let counter = 0;
   const setCounter = (count: number) => {
     counter = count;
@@ -6,4 +6,4 @@ export function setupCounter(element: HTMLButtonElement) {
   };
   element.addEventListener('click', () => setCounter(counter + 1));
   setCounter(0);
-}
+};
