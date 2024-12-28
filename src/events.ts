13def8d7a9568447986d9d4cc9fdf14cd6c5283e
Fabien <fabien.giannesini@gmail.com>
2024-12-28 17:25:36 +0100

    Extract method createEvent

--- src/events.ts
+++ src/events.ts
@@ -1,4 +1,4 @@
-import { State } from './user-story.ts';
+import { State, UserStory } from './user-story.ts';
 
 export interface TimeEvent {
   time: number;
@@ -6,3 +6,10 @@ export interface TimeEvent {
   state: State;
   thread: number;
 }
+
+export const createEvent = (time: number, userStory: UserStory): TimeEvent => ({
+  time: time,
+  userStoryName: userStory.name,
+  thread: userStory.thread!,
+  state: userStory.state,
+});
--- src/team.ts
+++ src/team.ts
@@ -1,5 +1,5 @@
 import { Backlog } from './backlog.ts';
-import { TimeEvent } from './events.ts';
+import { createEvent, TimeEvent } from './events.ts';
 import { idle, State, UserStory } from './user-story.ts';
 
 export interface Team {}
@@ -8,10 +8,13 @@ export type Thread = {
   id: number;
 };
 export class ParallelTeam implements Team {
-  private readonly devs: Thread[] = [];
-  constructor(devCount: number) {
+  private readonly _devs: Thread[] = [];
+  private readonly _review: boolean;
+
+  constructor(devCount: number, review: boolean) {
+    this._review = review;
     for (let i = 0; i < devCount; i++) {
-      this.devs.push({ id: i });
+      this._devs.push({ id: i });
     }
   }
 
@@ -23,7 +26,7 @@ export class ParallelTeam implements Team {
     const events: TimeEvent[] = [];
     let time = 1;
     while (backlog.hasMoreUserStories()) {
-      for (let dev of this.devs) {
+      for (let dev of this._devs) {
         let userStory: UserStory = backlog.next(dev);
         if (userStory == idle) {
           events.push({
@@ -38,25 +41,17 @@ export class ParallelTeam implements Team {
         inProgress.progression = userStory.progression + 1;
         inProgress.thread = dev.id;
         inProgress.state = State.IN_PROGRESS;
-        events.push({
-          time: time,
-          userStoryName: userStory.name,
-          thread: inProgress.thread,
-          state: inProgress.state,
-        });
-        if (inProgress.complexity == inProgress.progression) {
-          let done: UserStory = { ...inProgress };
-          done.state = State.DONE;
-          done.thread = dev.id;
-          events.push({
-            time: time,
-            userStoryName: userStory.name,
-            thread: done.thread,
-            state: done.state,
-          });
-          backlog.add(done);
-        } else {
-          backlog.add(inProgress);
+        events.push(createEvent(time, inProgress));
+        if (!this._review) {
+          if (inProgress.complexity == inProgress.progression) {
+            let done: UserStory = { ...inProgress };
+            done.state = State.DONE;
+            done.thread = dev.id;
+            events.push(createEvent(time, done));
+            backlog.add(done);
+          } else {
+            backlog.add(inProgress);
+          }
         }
       }
       time++;
@@ -68,13 +63,19 @@ export class ParallelTeam implements Team {
 
 class ParallelTeamBuilder {
   private devCount: number = 0;
+  private review: boolean = false;
 
   public withDevCount(devCount: number): ParallelTeamBuilder {
     this.devCount = devCount;
     return this;
   }
 
+  public withReview(): ParallelTeamBuilder {
+    this.review = true;
+    return this;
+  }
+
   public build(): ParallelTeam {
-    return new ParallelTeam(this.devCount);
+    return new ParallelTeam(this.devCount, this.review);
   }
 }
