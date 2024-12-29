d285f70df26d5e57465ca7ae586ab1777c0bbaef
Fabien <fabien.giannesini@gmail.com>
2024-12-29 14:14:20 +0100

    Create flow and main

--- src/main.ts
+++ src/main.ts
@@ -1,42 +1,6 @@
 import './style.scss';
-import { render } from './render/render.ts';
-import { State } from './compute/user-story.ts';
-
-render([
-  {
-    time: 1,
-    userStoryName: 'userStory1',
-    thread: 0,
-    state: State.IN_PROGRESS,
-  },
-  {
-    time: 1,
-    userStoryName: 'userStory1',
-    thread: 0,
-    state: State.TO_REVIEW,
-  },
-  {
-    time: 1,
-    userStoryName: 'idle',
-    thread: 1,
-    state: State.DONE,
-  },
-  {
-    time: 2,
-    userStoryName: 'idle',
-    thread: 0,
-    state: State.DONE,
-  },
-  {
-    time: 2,
-    userStoryName: 'userStory1',
-    thread: 1,
-    state: State.REVIEW,
-  },
-  {
-    time: 2,
-    userStoryName: 'userStory1',
-    thread: 1,
-    state: State.DONE,
-  },
-]);
+document
+  .querySelector<HTMLButtonElement>('button')!
+  .addEventListener('click', () => {
+    window.location.href = '/render/flow.html';
+  });
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,15 @@
-/// <reference types="vitest/config" />
-import { defineConfig } from 'vite';
+import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
+  root: './src',
+  build: {
+    rollupOptions: {
+      input: {
+        main: './src/index.html',
+        flow: './src/render/flow.html',
+      },
+    },
+  },
   test: {
     globals: true,
     environment: 'jsdom',
