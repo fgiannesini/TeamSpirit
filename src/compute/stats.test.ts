447879b37f97cd08cbe1577fd25a8abb410b8636
Fabien Giannesini <fabien.giannesini@gmail.com>
2025-03-04 11:20:34 +0100

    should compute a long lead time on one task by one dev

--- src/compute/stats.test.ts
+++ src/compute/stats.test.ts
@@ -1,8 +1,33 @@
 import { describe, expect, test } from 'vitest';
 import { computeStatEvents } from './stats.ts';
+import { State } from './user-story.ts';
 
 describe('stats', () => {
-  test('create a stat', () => {
-    expect(computeStatEvents([])).toEqual([{ time: 0, leadTime: 1 }]);
+  test('should compute a long lead time on one task by one dev', () => {
+    expect(
+      computeStatEvents([
+        {
+          time: 1,
+          userStoryName: 'userStory1',
+          thread: 0,
+          state: State.IN_PROGRESS,
+        },
+        {
+          time: 2,
+          userStoryName: 'userStory1',
+          thread: 0,
+          state: State.IN_PROGRESS,
+        },
+        {
+          time: 2,
+          userStoryName: 'userStory1',
+          thread: 0,
+          state: State.DONE,
+        },
+      ])
+    ).toEqual([
+      { time: 1, leadTime: 1 },
+      { time: 2, leadTime: 2 },
+    ]);
   });
 });
--- src/compute/stats.ts
+++ src/compute/stats.ts
@@ -5,9 +5,35 @@ export type StatEvent = {
   leadTime: number;
 };
 
-export const computeStatEvents = (timeEvents: TimeEvent[]): StatEvent[] => [
-  {
-    time: 0,
-    leadTime: 1,
-  },
-];
+const mean = (times: number[]) => {
+  const somme = times.reduce((acc, val) => acc + val, 0);
+  return somme / times.length;
+};
+
+export const computeStatEvents = (timeEvents: TimeEvent[]): StatEvent[] => {
+  const timesByUserStoryName = new Map<string, number>();
+  const statEvents = [];
+  let time = 1;
+  while (true) {
+    const currentEvents = timeEvents.filter(
+      (event: TimeEvent) => event.time == time
+    );
+    if (currentEvents.length === 0) break;
+
+    const uniqueUserStoryNames = new Set(
+      currentEvents.map((event) => event.userStoryName)
+    );
+    for (const userStoryName of uniqueUserStoryNames) {
+      const currentTime = timesByUserStoryName.get(userStoryName) ?? 0;
+      timesByUserStoryName.set(userStoryName, currentTime + 1);
+    }
+    const leadTime = mean(Array.from(timesByUserStoryName.values()));
+    statEvents.push({
+      time,
+      leadTime: leadTime,
+    });
+    time++;
+  }
+
+  return statEvents;
+};
