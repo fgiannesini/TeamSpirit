cc8421ce22986af9467cb4615b83770f892bf651
Fabien Giannesini <fabien.giannesini@gmail.com>
2025-03-04 11:22:33 +0100

    should compute a lead time on two tasks by two devs

--- src/compute/stats.test.ts
+++ src/compute/stats.test.ts
@@ -30,4 +30,35 @@ describe('stats', () => {
       { time: 2, leadTime: 2 },
     ]);
   });
+
+  test('should compute a lead time on two tasks by two devs', () => {
+    expect(
+      computeStatEvents([
+        {
+          time: 1,
+          userStoryName: 'userStory1',
+          thread: 0,
+          state: State.IN_PROGRESS,
+        },
+        {
+          time: 1,
+          userStoryName: 'userStory1',
+          thread: 0,
+          state: State.DONE,
+        },
+        {
+          time: 1,
+          userStoryName: 'userStory2',
+          thread: 1,
+          state: State.IN_PROGRESS,
+        },
+        {
+          time: 1,
+          userStoryName: 'userStory2',
+          thread: 1,
+          state: State.DONE,
+        },
+      ])
+    ).toEqual([{ time: 1, leadTime: 1 }]);
+  });
 });
