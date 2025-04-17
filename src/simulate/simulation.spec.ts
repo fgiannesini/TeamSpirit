37d8d1adf063822393ffb11ce7cfdb78185b9caf
Fabien <fabien.giannesini@gmail.com>
2025-04-17 11:41:16 +0200

    Should generate a bug during the first and second turn

--- src/simulate/simulation.spec.ts
+++ src/simulate/simulation.spec.ts
@@ -33,6 +33,38 @@ describe('Simulation', () => {
     expect(structureEvents.length).toBeGreaterThan(0);
   });
 
+  test('Should generate a bug during the first and second turn', () => {
+    const team = new EnsembleTeam([{ id: 0, name: 'thread0', power: 1 }]);
+    const backlog = new Backlog([
+      todo({
+        id: 0,
+        name: 'US1',
+        complexity: 2,
+      }),
+    ]);
+    const randomMock = vitest
+      .fn()
+      .mockReturnValueOnce(0)
+      .mockReturnValueOnce(0.05)
+      .mockReturnValue(1);
+    const { structureEvents } = simulate(backlog, team, randomMock);
+    expect(structureEvents.slice(-2)).toEqual([
+      {
+        id: 1,
+        name: 'bug-0',
+        time: 1,
+        action: 'CreateUserStory',
+      },
+      {
+        id: 2,
+        name: 'bug-1',
+        time: 2,
+        action: 'CreateUserStory',
+      },
+    ]);
+    expect(backlog.userStoriesDone.length).toEqual(3);
+  });
+
   test('Should generate a bug during the second turn', () => {
     const team = new EnsembleTeam([{ id: 0, name: 'thread0', power: 1 }]);
     const backlog = new Backlog([
--- src/simulate/simulation.ts
+++ src/simulate/simulation.ts
@@ -1,6 +1,7 @@
 import {
   type Backlog,
   addUserStory,
+  getUserStories,
   hasMoreUserStories,
   shouldGenerateBug,
 } from './backlog.ts';
@@ -17,19 +18,22 @@ export const simulate = (
 ) => {
   const timeEvents: TimeEvent[] = [];
   let time = 1;
+  let bugCount = 0;
   const structureEvents = structureEventsOnInitialization(backlog, team);
   while (hasMoreUserStories(backlog)) {
     if (shouldGenerateBug(random)) {
+      const id = getUserStories(backlog).length;
+      const name = `bug-${bugCount}`;
       structureEvents.push({
         time,
-        name: 'bug-0',
-        id: 2,
+        name,
+        id,
         action: 'CreateUserStory',
       });
       addUserStory(
         {
-          id: 2,
-          name: 'bug-0',
+          id,
+          name,
           complexity: 1,
           reviewComplexity: 1,
           progression: 0,
@@ -39,6 +43,7 @@ export const simulate = (
         },
         backlog,
       );
+      bugCount++;
     }
     timeEvents.push(...simulateTimeEvents(team, backlog, time));
     time++;
