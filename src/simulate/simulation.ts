255b4969ebb9e1bb1aae2b65fb15ca9f64465379
Fabien Giannesini <fabien.giannesini@gmail.com>
2025-06-04 20:07:13 +0200

    apply bug detection on each done user stories

--- src/simulate/simulation.ts
+++ src/simulate/simulation.ts
@@ -21,34 +21,36 @@ export const simulate = (
   let bugCount = 0;
   const structureEvents = structureEventsOnInitialization(backlog, team);
   while (hasMoreUserStories(backlog)) {
-    if (
-      backlog.userStoriesDone.length > 0 &&
-      shouldGenerateBug(randomProvider, backlog.userStoriesDone[0], team, time)
-    ) {
-      const id = getUserStories(backlog).length;
-      const name = `bug-${bugCount}`;
-      structureEvents.push({
-        time,
-        name,
-        id,
-        action: 'CreateUserStory',
-      });
-      addUserStory(
-        {
-          id,
+    backlog.userStoriesDone
+      .map((userStory) =>
+        shouldGenerateBug(randomProvider, userStory, team, time),
+      )
+      .filter((result) => result)
+      .forEach(() => {
+        const id = getUserStories(backlog).length;
+        const name = `bug-${bugCount}`;
+        structureEvents.push({
+          time,
           name,
-          complexity: 1,
-          reviewComplexity: 1,
-          progression: 0,
-          review: noReview,
-          threadId: undefined,
-          state: 'Todo',
-          timeDone: 0,
-        },
-        backlog,
-      );
-      bugCount++;
-    }
+          id,
+          action: 'CreateUserStory',
+        });
+        addUserStory(
+          {
+            id,
+            name,
+            complexity: 1,
+            reviewComplexity: 1,
+            progression: 0,
+            review: noReview,
+            threadId: undefined,
+            state: 'Todo',
+            timeDone: 0,
+          },
+          backlog,
+        );
+        bugCount++;
+      });
     timeEvents.push(...simulateTimeEvents(team, backlog, time));
     time++;
   }
