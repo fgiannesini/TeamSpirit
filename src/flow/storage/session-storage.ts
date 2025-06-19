f52ddfa01dce4dc9b401ff767b1e6f57348406d9
Fabien Giannesini <fabien.giannesini@gmail.com>
2025-06-19 10:50:10 +0200

    fix warning of biomejs in session-storage.ts

--- src/flow/storage/session-storage.ts
+++ src/flow/storage/session-storage.ts
@@ -2,27 +2,27 @@ import type { TimeEvent } from '../../simulate/events.ts';
 import type { StructureEvent } from '../../simulate/simulation-structure.ts';
 import type { StatEvent } from '../../simulate/stats.ts';
 
-export const loadTimeEvents = (key: string) =>
+export const loadTimeEvents = (key: string): TimeEvent[] =>
   JSON.parse(
     sessionStorage.getItem(`computation-${key}`) ?? '[]',
   ) as TimeEvent[];
 
-export const saveTimeEvents = (timeEvents: TimeEvent[], key: string) =>
+export const saveTimeEvents = (timeEvents: TimeEvent[], key: string): void =>
   sessionStorage.setItem(`computation-${key}`, JSON.stringify(timeEvents));
 
-export const loadStatEvents = (key: string) =>
+export const loadStatEvents = (key: string): StatEvent[] =>
   JSON.parse(sessionStorage.getItem(`stats-${key}`) ?? '[]') as StatEvent[];
 
-export const saveStatEvents = (statEvents: StatEvent[], key: string) =>
+export const saveStatEvents = (statEvents: StatEvent[], key: string): void =>
   sessionStorage.setItem(`stats-${key}`, JSON.stringify(statEvents));
 
 export const saveStructureEvents = (
   structureEvents: StructureEvent[],
   key: string,
-) =>
+): void =>
   sessionStorage.setItem(`structure-${key}`, JSON.stringify(structureEvents));
 
-export const loadStructureEvents = (key: string) =>
+export const loadStructureEvents = (key: string): StructureEvent[] =>
   JSON.parse(
     sessionStorage.getItem(`structure-${key}`) ?? '[]',
   ) as StructureEvent[];
