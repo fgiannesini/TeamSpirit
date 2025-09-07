4f000c86b5a2fd7511d23841f2ac2aab56724b31
Fabien Giannesini <fabien.giannesini@gmail.com>
2025-09-07 12:33:04 +0200

    Should have a button to add a developer && Should generate a card to add a developer

--- src/front/form.test.ts
+++ src/front/form.test.ts
@@ -1,10 +1,31 @@
-import { mount } from '@vue/test-utils';
+import { mount, type VueWrapper } from '@vue/test-utils';
 import { describe, expect, test } from 'vitest';
 import Form from './form.vue';
 
 describe('Form', () => {
+  const createWrapper = (): VueWrapper => {
+    return mount(Form);
+  };
   test('Should render a form', () => {
-    const wrapper = mount(Form);
+    const wrapper = createWrapper();
     expect(wrapper.exists()).toBe(true);
+  });
+
+  test('Should have a main', () => {
+    const wrapper = createWrapper();
+    expect(wrapper.get('main').isVisible()).toBe(true);
+  });
+
+  test('Should have a button to add a developer', () => {
+    const wrapper = createWrapper();
+    expect(wrapper.get('[data-testid=add-developer-button]').isVisible()).toBe(
+      true,
+    );
+  });
+
+  test('Should generate a card to add a developer', () => {
+    const wrapper = createWrapper();
+    wrapper.get('[data-testid=add-developer-button]').trigger('click');
+    expect(wrapper.get('[data-testid=developer-card]').isVisible()).toBe(true);
   });
 });
--- src/front/form.vue
+++ src/front/form.vue
@@ -1,5 +1,11 @@
 <template>
-  <div id="form">
-    <p>This is now a Vue component!</p>
-  </div>
+  <main class="responsive">
+    <button class="small-round extra" data-testid="add-developer-button">
+      <i>add</i>
+      <span>Add a developer</span>
+    </button>
+    <article data-testid="developer-card">
+    </article>
+
+  </main>
 </template>
