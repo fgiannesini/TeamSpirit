35463cfe42631eec27fb332f64ee8bb3f7f2a664
Fabien Giannesini <fabien.giannesini@gmail.com>
2025-09-07 16:06:36 +0200

    Developer: Should have a button to remove the developer

--- src/front/developer-card.test.ts
+++ src/front/developer-card.test.ts
@@ -7,4 +7,11 @@ describe('DeveloperCard', () => {
     const wrapper = mount(DeveloperCard);
     expect(wrapper.exists()).toBe(true);
   });
+
+  test('Should have a button to remove the developer', () => {
+    const wrapper = mount(DeveloperCard);
+    expect(
+      wrapper.get('[data-testid=remove-developer-button]').isVisible(),
+    ).toBe(true);
+  });
 });
--- src/front/developer-card.vue
+++ src/front/developer-card.vue
@@ -1,6 +1,9 @@
 <template>
   <article>
-
+<button class="small-round extra" data-testid="remove-developer-button">
+  <i>remove</i>
+  <span>Remove</span>
+</button>
   </article>
 </template>
 <script setup lang="ts">
