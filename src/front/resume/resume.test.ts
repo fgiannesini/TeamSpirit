cc4ec649e522cd290a17002abb3fc174aff0588a
Fabien Giannesini <fabien.giannesini@gmail.com>
2025-09-12 09:51:32 +0200

    Resume: Should display team mode

--- src/front/resume/resume.test.ts
+++ src/front/resume/resume.test.ts
@@ -1,14 +1,32 @@
+import { createTestingPinia } from '@pinia/testing';
 import { shallowMount, type VueWrapper } from '@vue/test-utils';
 import { describe, expect, test } from 'vitest';
+import type { State } from '../form-store.ts';
 import Resume from './resume.vue';
 
 describe('Resume', () => {
-  const createWrapper = (): VueWrapper => {
-    return shallowMount(Resume);
+  const createWrapper = (state: Partial<State> = {}): VueWrapper => {
+    return shallowMount(Resume, {
+      global: {
+        plugins: [
+          createTestingPinia({
+            initialState: {
+              form: { ...state },
+            },
+          }),
+        ],
+      },
+    });
   };
 
   test('Should render the component', () => {
     const wrapper = createWrapper();
     expect(wrapper.exists()).toBe(true);
+  });
+
+  test('Should display team mode', () => {
+    const wrapper = createWrapper({ teamMode: 'random' });
+    const teamMode = wrapper.get('[data-testid=team-mode]');
+    expect(teamMode.text()).toBe('random');
   });
 });
--- src/front/resume/resume.vue
+++ src/front/resume/resume.vue
@@ -1,1 +1,8 @@
-<template></template>
+<script setup lang="ts">
+import {useFormStore} from "../form-store.ts";
+
+const formStore = useFormStore();
+</script>
+<template>
+  <span data-testid="team-mode">{{formStore.$state.teamMode}}</span>
+</template>
