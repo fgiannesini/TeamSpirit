e5ca86437bd568fe4b186f0868effbf75e7f76f0
Fabien Giannesini <fabien.giannesini@gmail.com>
2025-10-13 14:04:12 +0200

    Reviewers: Should render a slider

--- src/front/reviewers/reviewers.test.ts
+++ src/front/reviewers/reviewers.test.ts
@@ -1,5 +1,6 @@
 import { shallowMount, type VueWrapper } from '@vue/test-utils';
 import { describe, expect, test } from 'vitest';
+import Slider from '../slider.vue';
 import Reviewers from './reviewers.vue';
 
 describe('Reviewers', () => {
@@ -9,5 +10,15 @@ describe('Reviewers', () => {
   test('Should render the component', () => {
     const wrapper = createWrapper();
     expect(wrapper.exists()).toBe(true);
+  });
+
+  test('Should render a slider', () => {
+    const wrapper = createWrapper();
+    const slider = wrapper.getComponent(Slider);
+    expect(slider.props()).toStrictEqual({
+      min: 0,
+      max: 1,
+      value: 0,
+    });
   });
 });
--- src/front/reviewers/reviewers.vue
+++ src/front/reviewers/reviewers.vue
@@ -1,1 +1,6 @@
-<template></template>
+<template>
+  <slider :min="0" :max="1" :value="0"/>
+</template>
+<script setup lang="ts">
+import Slider from "../slider.vue";
+</script>
