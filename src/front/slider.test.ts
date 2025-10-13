db1795199b25c9ef8407f801947f8cde24f9a0e3
Fabien Giannesini <fabien.giannesini@gmail.com>
2025-10-13 13:44:52 +0200

    Slider: copy features from developer-card

--- src/front/slider.test.ts
+++ src/front/slider.test.ts
@@ -1,14 +1,58 @@
 import { shallowMount, type VueWrapper } from '@vue/test-utils';
-import { describe, expect, it } from 'vitest';
+import { describe, expect, it, test } from 'vitest';
 import Slider from './slider.vue';
 
 describe('Slider', () => {
   const createWrapper = (): VueWrapper => {
-    return shallowMount(Slider);
+    return shallowMount(Slider, {
+      props: {
+        max: 7,
+        min: 1,
+        value: 3,
+      },
+    });
   };
 
   it('Should render', () => {
     const wrapper = createWrapper();
     expect(wrapper.exists()).toBe(true);
+  });
+
+  test('Should render a range', () => {
+    const wrapper = createWrapper();
+    const range = wrapper.get('[data-testid=range]');
+    expect(range.isVisible()).toBe(true);
+  });
+
+  test('Should have the tooltip rendered', () => {
+    const wrapper = createWrapper();
+    const tooltip = wrapper.get('[data-testid=range-tooltip]');
+    expect(tooltip.isVisible()).toBe(true);
+  });
+
+  test('Should show minimum of the range', () => {
+    const wrapper = createWrapper();
+    const minimum = wrapper.get('[data-testid=minimum]');
+    expect(minimum.text()).toBe('1');
+  });
+
+  test('Should show maximum of the range', () => {
+    const wrapper = createWrapper();
+    const minimum = wrapper.get('[data-testid=maximum]');
+    expect(minimum.text()).toBe('7');
+  });
+
+  test('Should bind value', () => {
+    const wrapper = createWrapper();
+    const input = wrapper.get<HTMLInputElement>('[data-testid=input]');
+    expect(input.element.value).toBe('3');
+  });
+
+  test('Should send an update event on value change', () => {
+    const wrapper = createWrapper();
+    const input = wrapper.get<HTMLInputElement>('[data-testid=input]');
+    input.setValue(2);
+    const emitted = wrapper.emitted('update:value');
+    expect(emitted?.[0]).toStrictEqual([2]);
   });
 });
--- src/front/slider.vue
+++ src/front/slider.vue
@@ -1,9 +1,25 @@
 <script setup lang="ts">
 
+import type {InputHTMLAttributes} from "vue";
+defineProps<{
+  min:number;
+  max:number;
+  value:number;
+}>()
+defineEmits(['update:value']);
 </script>
 
 <template>
-
+  <nav>
+    <span data-testid="minimum">{{ min }}</span>
+    <label class="slider" data-testid="range">
+      <input data-testid="input" type="range" :value="value"
+             @input="$emit('update:value', Number(($event.target as InputHTMLAttributes).value))" :min="min" :max="max">
+      <span></span>
+      <div class="tooltip" data-testid=range-tooltip></div>
+    </label>
+    <span data-testid="maximum">{{ max }}</span>
+  </nav>
 </template>
 
 <style scoped lang="scss">
