7eccb9fa05412bde3002663a8757a8255cb18166
Fabien Giannesini <fabien.giannesini@gmail.com>
2025-11-01 21:47:53 +0100

    Selector: implements selection feature and emit event on change

--- src/front/selector.test.ts
+++ src/front/selector.test.ts
@@ -1,14 +1,85 @@
-import { shallowMount, type VueWrapper } from '@vue/test-utils';
+import {
+  type DOMWrapper,
+  shallowMount,
+  type VueWrapper,
+} from '@vue/test-utils';
 import { describe, expect, test } from 'vitest';
 import Selector from './selector.vue';
 
 describe('Selector', () => {
   const createWrapper = (): VueWrapper => {
-    return shallowMount(Selector);
+    return shallowMount(Selector, {
+      props: {
+        selectorMode: undefined,
+      },
+    });
   };
 
   test('should render', () => {
     const wrapper = createWrapper();
     expect(wrapper.exists()).toBe(true);
+  });
+
+  const radio = (
+    wrapper: VueWrapper,
+    radioName: string,
+  ): DOMWrapper<HTMLInputElement> => {
+    return wrapper.find<HTMLInputElement>(`[data-testid=${radioName}-radio]`);
+  };
+
+  test('Should have a radio not checked by default button for custom mode', () => {
+    const wrapper = createWrapper();
+    const customRadio = radio(wrapper, 'custom');
+    expect(customRadio.isVisible()).toBe(true);
+    expect(customRadio.element.checked).toBe(false);
+  });
+
+  test('Should have a radio not checked by default button for random mode', () => {
+    const wrapper = createWrapper();
+    const randomRadio = radio(wrapper, 'random');
+    expect(randomRadio.isVisible()).toBe(true);
+    expect(randomRadio.element.checked).toBe(false);
+  });
+
+  test('Should select random mode', async () => {
+    const wrapper = createWrapper();
+    const randomRadio = radio(wrapper, 'random');
+    await randomRadio.trigger('click');
+    expect(randomRadio.element.checked).toBe(true);
+  });
+
+  test('Should emit en event on random mode selection', async () => {
+    const wrapper = createWrapper();
+    await radio(wrapper, 'random').trigger('click');
+    expect(wrapper.emitted('select')).toStrictEqual([['random']]);
+  });
+
+  test('Should emit en event on random mode selection', async () => {
+    const wrapper = createWrapper();
+    await radio(wrapper, 'custom').trigger('click');
+    expect(wrapper.emitted('select')).toStrictEqual([['custom']]);
+  });
+
+  test('Should select random mode, then custom mode', async () => {
+    const wrapper = createWrapper();
+    const randomRadio = radio(wrapper, 'random');
+    await randomRadio.trigger('click');
+
+    await radio(wrapper, 'custom').trigger('click');
+    expect(randomRadio.element.checked).toBe(false);
+  });
+
+  test('Should select custom mode on initialisation', async () => {
+    const wrapper = createWrapper();
+    await wrapper.setProps({ selectorMode: 'custom' });
+    const customRadio = radio(wrapper, 'custom');
+    expect(customRadio.element.checked).toBe(true);
+  });
+
+  test('Should select random mode on initialisation', async () => {
+    const wrapper = createWrapper();
+    await wrapper.setProps({ selectorMode: 'random' });
+    const randomRadio = radio(wrapper, 'random');
+    expect(randomRadio.element.checked).toBe(true);
   });
 });
--- src/front/selector.vue
+++ src/front/selector.vue
@@ -1,1 +1,21 @@
-<template></template>
+<script setup lang="ts">
+type SelectorMode = 'random' | 'custom' | undefined;
+
+defineProps<{
+  selectorMode: SelectorMode;
+}>()
+
+defineEmits(['select']);
+</script>
+<template>
+  <nav>
+    <label class="radio">
+      <input data-testid="random-radio" type="radio" name="team" @click="$emit('select', 'random')" :checked="selectorMode === 'random'">
+      <span>Random</span>
+    </label>
+    <label class="radio">
+      <input data-testid="custom-radio" type="radio" name="team" @click="$emit('select', 'custom')" :checked="selectorMode === 'custom'">
+      <span>Custom</span>
+    </label>
+  </nav>
+</template>
