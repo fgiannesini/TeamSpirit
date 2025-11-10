f9cb64ffdf7412874498186eab0d7dee89e7c2e5
Fabien Giannesini <fabien.giannesini@gmail.com>
2025-11-10 18:48:38 +0100

    Period Card: Improve style

--- src/front/period-card.vue
+++ src/front/period-card.vue
@@ -1,9 +1,18 @@
 <template>
-  <h4 data-testid="title" class="max">Period {{ id }}</h4>
-  <input data-testid="start-date-input" type="date"/>
-  <label data-testid="start-date-label">Start</label>
-  <input data-testid="end-date-input" type="date"/>
-  <label data-testid="end-date-label">End</label>
+  <article class="medium-width">
+    <nav>
+      <h4 data-testid="title" class="max">Period {{ id }}</h4>
+    </nav>
+    <div class="space"/>
+    <div class="field label border fill">
+      <input data-testid="start-date-input" type="date"/>
+      <label data-testid="start-date-label">Start</label>
+    </div>
+    <div class="field label border fill">
+      <input data-testid="end-date-input" type="date"/>
+      <label data-testid="end-date-label">End</label>
+    </div>
+  </article>
 </template>
 <script setup lang="ts">
 defineProps<{ id: number }>()
