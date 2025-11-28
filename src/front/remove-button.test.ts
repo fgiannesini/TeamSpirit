fecac6fa0bb6ee6456f3936316a67b2f2ffe6fbe
Fabien Giannesini <fabien.giannesini@gmail.com>
2025-11-28 11:06:42 +0100

    Create component Remove button

---
+++ src/front/remove-button.test.ts
@@ -0,0 +1,21 @@
+import { shallowMount, type VueWrapper } from '@vue/test-utils';
+import { describe, expect, test } from 'vitest';
+import RemoveButton from './remove-button.vue';
+
+describe('Remove button', () => {
+  const createWrapper = (): VueWrapper => {
+    return shallowMount(RemoveButton);
+  };
+
+  test('Should display a button', () => {
+    const wrapper = createWrapper();
+    expect(wrapper.get('[data-testid=remove-button]').isVisible()).toBe(true);
+  });
+
+  test('Should send a remove event on click on button', () => {
+    const wrapper = createWrapper();
+    const button = wrapper.get('[data-testid=remove-button]');
+    button.trigger('click');
+    expect(wrapper.emitted()).toHaveProperty('remove');
+  });
+});
---
+++ src/front/remove-button.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <button
+      class="small-round small secondary"
+      data-testid="remove-button"
+      @click="$emit('remove')"
+  >
+    <i>remove</i>
+    <span>Remove</span>
+  </button>
+</template>
