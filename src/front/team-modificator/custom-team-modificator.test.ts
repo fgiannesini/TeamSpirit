19ecbdbbdc1bfcecdb69c331d74f933ec85510c0
Fabien Giannesini <fabien.giannesini@gmail.com>
2025-11-28 11:58:38 +0100

    Custom Team modification: add empty state test

--- src/front/team-modificator/custom-team-modificator.test.ts
+++ src/front/team-modificator/custom-team-modificator.test.ts
@@ -1,5 +1,6 @@
 import { shallowMount, type VueWrapper } from '@vue/test-utils';
 import { describe, expect, test } from 'vitest';
+import AddButton from '../add-button.vue';
 import PeriodCard from '../period-card.vue';
 import CustomTeamModificator from './custom-team-modificator.vue';
 
@@ -11,6 +12,17 @@ describe('Custom Team Modificator', () => {
   test('Should render', () => {
     const wrapper = createWrapper();
     expect(wrapper.exists()).toBe(true);
+  });
+
+  describe('Empty state', () => {
+    test('Should render empty state', () => {
+      const wrapper = createWrapper();
+      expect(wrapper.find('[data-testid=empty-state]').exists()).toBe(true);
+    });
+    test('Should have a button to add a team modification when no team modification is added', () => {
+      const wrapper = createWrapper();
+      expect(wrapper.findComponent(AddButton).isVisible()).toBe(true);
+    });
   });
 
   test('Should render a period card', () => {
--- src/front/team-modificator/custom-team-modificator.vue
+++ src/front/team-modificator/custom-team-modificator.vue
@@ -1,6 +1,18 @@
 <template>
+  <article
+      class="medium middle-align center-align"
+      data-testid="empty-state"
+  >
+    <div>
+      <i class="extra">group_remove</i>
+      <h5>No team modification exists</h5>
+      <div class="medium-space"></div>
+      <add-button text="Add a team modification"/>
+    </div>
+  </article>
   <PeriodCard :id="0"/>
 </template>
 <script setup lang="ts">
 import PeriodCard from "../period-card.vue";
+import AddButton from "../add-button.vue";
 </script>
