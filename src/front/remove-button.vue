ab3c2134ea7cfba2281d1f4a58920f6ba7d86a1a
Fabien <fabien.giannesini@gmail.com>
2025-12-24 16:12:06 +0100

    Remove button: make icon transparent and circle

--- src/front/remove-button.vue
+++ src/front/remove-button.vue
@@ -1,13 +1,8 @@
 <script setup lang="ts">
-
 </script>
 
 <template>
-  <button
-      class="small-round small secondary"
-      data-testid="remove-button"
-  >
-    <i>remove</i>
-    <span>Remove</span>
+  <button class="transparent circle" data-testid="remove-button">
+    <i>delete</i>
   </button>
 </template>
