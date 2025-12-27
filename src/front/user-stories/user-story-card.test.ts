7bcc5fcc36e1786d9f9d19ead96bfac401b88760
Fabien <fabien.giannesini@gmail.com>
2025-12-27 18:59:28 +0100

    User story card: Add complexity slider

--- src/front/user-stories/user-story-card.test.ts
+++ src/front/user-stories/user-story-card.test.ts
@@ -1,6 +1,7 @@
-import { shallowMount, type VueWrapper } from '@vue/test-utils';
+import { flushPromises, shallowMount, type VueWrapper } from '@vue/test-utils';
 import { describe, expect, test } from 'vitest';
 import RemoveButton from '../remove-button.vue';
+import Slider from '../slider.vue';
 import UserStoryCard from './user-story-card.vue';
 
 describe('User Story Card', () => {
@@ -8,6 +9,7 @@ describe('User Story Card', () => {
     return shallowMount(UserStoryCard, {
       props: {
         id: 1,
+        complexity: 3,
       },
     });
   };
@@ -32,5 +34,33 @@ describe('User Story Card', () => {
     const button = wrapper.getComponent(RemoveButton);
     button.trigger('click');
     expect(wrapper.emitted()).toHaveProperty('remove');
+  });
+
+  describe('Complexity', () => {
+    test('Should have a label', () => {
+      const wrapper = createWrapper();
+      const label = wrapper.get('[data-testid=complexity-label]');
+      expect(label.text()).toBe('Complexity');
+    });
+
+    test('Should bind slider properties', () => {
+      const wrapper = createWrapper();
+      const slider = wrapper.getComponent(Slider);
+      expect(slider.props()).toStrictEqual({
+        min: 1,
+        max: 10,
+        value: 3,
+      });
+    });
+
+    test('Should send an update event on complexity change', async () => {
+      const wrapper = createWrapper();
+      const slider = wrapper.getComponent(Slider);
+      slider.vm.$emit('update:value', 2);
+      await flushPromises();
+
+      const emitted = wrapper.emitted('update:complexity');
+      expect(emitted?.[0]).toStrictEqual([2]);
+    });
   });
 });
--- src/front/user-stories/user-story-card.vue
+++ src/front/user-stories/user-story-card.vue
@@ -1,8 +1,11 @@
 <script setup lang="ts">
 import RemoveButton from '../remove-button.vue';
+import Slider from '../slider.vue';
 
+defineEmits(['remove', 'update:complexity']);
 defineProps<{
   id: number;
+  complexity: number;
 }>();
 </script>
 
@@ -12,5 +15,16 @@ defineProps<{
       <h4 data-testid="title" class="max">User story {{ id }}</h4>
       <remove-button @click="$emit('remove')"/>
     </nav>
+    <div class="space"/>
+    <fieldset>
+      <legend data-testid="complexity-label">Complexity</legend>
+      <div class="space"/>
+      <slider
+          :min="1"
+          :max="10"
+          :value="complexity"
+          @update:value="$emit('update:complexity', $event)"
+      />
+    </fieldset>
   </article>
 </template>
