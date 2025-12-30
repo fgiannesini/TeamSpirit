bfb427e1abe9c3d12e8eda3190a9fc5eba11e9e4
Fabien <fabien.giannesini@gmail.com>
2025-12-30 12:30:59 +0100

    Router: add route /simulate

--- src/front/router.ts
+++ src/front/router.ts
@@ -1,7 +1,11 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import Form from './form.vue';
+import Simulation from './simulation/simulation.vue';
 
-const routes = [{ path: '/main.html', component: Form }];
+const routes = [
+  { path: '/main.html', component: Form },
+  { path: '/simulate', component: Simulation },
+];
 
 export const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
