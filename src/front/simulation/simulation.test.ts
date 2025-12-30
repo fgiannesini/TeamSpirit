aa6a3f6e687bd145ae5534c76d428c09c14dac35
Fabien <fabien.giannesini@gmail.com>
2025-12-30 14:56:27 +0100

    Simulation: map developers

--- src/front/simulation/simulation.test.ts
+++ src/front/simulation/simulation.test.ts
@@ -2,10 +2,15 @@ import { createTestingPinia } from '@pinia/testing';
 import { shallowMount, type VueWrapper } from '@vue/test-utils';
 import { describe, expect, test, vi } from 'vitest';
 import { noBugGenerator } from '../../simulate/bug-generator.ts';
-import { createBacklog, parallelTeam } from '../../simulate/factory.ts';
+import {
+  createBacklog,
+  createThread,
+  parallelTeam,
+} from '../../simulate/factory.ts';
 import { noPriorityModificator } from '../../simulate/priority-modificator.ts';
 import { noTeamModificator } from '../../simulate/team-modificator.ts';
 import type { State } from '../form-store.ts';
+import { developer } from '../front-factory-for-test.ts';
 import Resume from '../resume/resume.vue';
 import Simulation from './simulation.vue';
 
@@ -53,13 +58,21 @@ describe('Simulation', () => {
       vi.mock('../../simulate/simulation.ts', () => ({
         simulate: simulateMock,
       }));
-      const wrapper = createWrapper();
+      const wrapper = createWrapper({
+        developers: [
+          developer({ id: 0, experience: 2 }),
+          developer({ id: 1, experience: 3 }),
+        ],
+      });
       const launchButton = wrapper.get('[data-testid=launch-button]');
       launchButton.trigger('click');
 
       expect(simulateMock).toHaveBeenCalledWith(
         createBacklog(),
-        parallelTeam(),
+        parallelTeam([
+          createThread({ id: 0, power: 2 }),
+          createThread({ id: 1, power: 3 }),
+        ]),
         noBugGenerator,
         noTeamModificator,
         noPriorityModificator,
--- src/front/simulation/simulation.vue
+++ src/front/simulation/simulation.vue
@@ -1,16 +1,28 @@
 <script setup lang="ts">
 import { noBugGenerator } from '../../simulate/bug-generator.ts';
-import { createBacklog, parallelTeam } from '../../simulate/factory.ts';
+import {
+  createBacklog,
+  createThread,
+  parallelTeam,
+} from '../../simulate/factory.ts';
 import { noPriorityModificator } from '../../simulate/priority-modificator.ts';
 import { simulate } from '../../simulate/simulation.ts';
 import { noTeamModificator } from '../../simulate/team-modificator.ts';
+import { useFormStore } from '../form-store.ts';
 import Resume from '../resume/resume.vue';
+
+let store = useFormStore();
+const team = parallelTeam(
+  store.developers.map((developer) =>
+    createThread({ id: developer.id, power: developer.experience }),
+  ),
+);
 </script>
 
 <template>
   <nav class="right" data-testid="resume-panel">
     Configuration
     <resume/>
-    <button data-testid="launch-button" @click="simulate(createBacklog(),parallelTeam(), noBugGenerator, noTeamModificator, noPriorityModificator)">Launch</button>
+    <button data-testid="launch-button" @click="simulate(createBacklog(),team, noBugGenerator, noTeamModificator, noPriorityModificator)">Launch</button>
   </nav>
 </template>
