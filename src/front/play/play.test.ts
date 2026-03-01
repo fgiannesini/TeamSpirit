f9e370806fca08da95c475412aa902e9e7760b66
Fabien <fabien.giannesini@gmail.com>
2026-03-01 10:49:03 +0100

    Play: Should render team type

--- src/front/play/play.test.ts
+++ src/front/play/play.test.ts
@@ -1,10 +1,36 @@
+import { createTestingPinia } from '@pinia/testing';
+import { shallowMount, type VueWrapper } from '@vue/test-utils';
 import { describe, expect, test } from 'vitest';
-import { shallowMount } from '@vue/test-utils';
+import type { State } from '../form-store.ts';
 import Play from './play.vue';
 
 describe('Play', () => {
-  test('Should render component', () => {
-    const wrapper = shallowMount(Play);
-    expect(wrapper.exists()).toBe(true);
+  const createWrapper = (state: Partial<State> = {}): VueWrapper => {
+    return shallowMount(Play, {
+      props: { id: 0 },
+      global: {
+        plugins: [
+          createTestingPinia({
+            initialState: {
+              form: { ...state },
+            },
+          }),
+        ],
+      },
+    });
+  };
+
+  test('Should render team type', () => {
+    const wrapper = createWrapper({
+      simulationOutputs: [
+        {
+          teamType: 'Parallel',
+          statEvents: [],
+          structureEvents: [],
+          timeEvents: [],
+        },
+      ],
+    });
+    expect(wrapper.text()).toBe('Parallel');
   });
 });
--- src/front/play/play.vue
+++ src/front/play/play.vue
@@ -1,6 +1,10 @@
 <script setup lang="ts">
+import { useFormStore } from '../form-store.ts';
+
+const props = defineProps<{ id: number }>();
+const store = useFormStore();
 </script>
 
 <template>
-
+{{store.simulationOutputs[props.id].teamType}}
 </template>
