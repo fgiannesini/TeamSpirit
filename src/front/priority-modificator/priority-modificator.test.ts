6146cfb5f07f81947ce585ef1710d794730e9cad
Fabien <fabien.giannesini@gmail.com>
2026-05-10 20:09:26 +0200

    Add priority modificator component

---
+++ src/front/priority-modificator/priority-modificator.test.ts
@@ -0,0 +1,86 @@
+import { createTestingPinia } from '@pinia/testing';
+import { flushPromises, shallowMount, type VueWrapper } from '@vue/test-utils';
+import { describe, expect, test } from 'vitest';
+import { type State, useFormStore } from '../form-store.ts';
+import Selector from '../shared/selector.vue';
+import CustomPriorityModificator from './custom-priority-modificator.vue';
+import PriorityModificator from './priority-modificator.vue';
+
+describe('Priority Modificator', () => {
+  const createWrapper = (state: Partial<State> = {}): VueWrapper => {
+    return shallowMount(PriorityModificator, {
+      global: {
+        plugins: [
+          createTestingPinia({
+            initialState: {
+              form: { ...state },
+            },
+          }),
+        ],
+      },
+    });
+  };
+
+  test('Should render', () => {
+    const wrapper = createWrapper();
+    expect(wrapper.exists()).toBe(true);
+  });
+
+  test('Should have a selector not selected by default', () => {
+    const wrapper = createWrapper();
+    const selector = wrapper.findComponent(Selector);
+    expect(selector.props()).toMatchObject({
+      selectedMode: 'notSet',
+    });
+  });
+
+  test('Should update random mode in store', async () => {
+    const wrapper = createWrapper();
+    wrapper.findComponent(Selector).vm.$emit('update:selectedMode', 'random');
+    await flushPromises();
+
+    const formStore = useFormStore();
+    expect(formStore.priorityModificatorMode).toStrictEqual('random');
+  });
+
+  test('Should select custom mode', async () => {
+    const wrapper = createWrapper();
+    wrapper.findComponent(Selector).vm.$emit('update:selectedMode', 'custom');
+    await flushPromises();
+
+    const formStore = useFormStore();
+    expect(formStore.priorityModificatorMode).toStrictEqual('custom');
+  });
+
+  test('Should select custom mode on loading', () => {
+    const wrapper = createWrapper({ priorityModificatorMode: 'custom' });
+
+    const selector = wrapper.findComponent(Selector);
+    expect(selector.props()).toMatchObject({
+      selectedMode: 'custom',
+    });
+  });
+
+  test('Should select random mode on loading', () => {
+    const wrapper = createWrapper({ priorityModificatorMode: 'random' });
+
+    const selector = wrapper.findComponent(Selector);
+    expect(selector.props()).toMatchObject({
+      selectedMode: 'random',
+    });
+  });
+
+  test('Should render Custom Priority Modificator', () => {
+    const wrapper = createWrapper({ priorityModificatorMode: 'custom' });
+    expect(wrapper.findComponent(CustomPriorityModificator).exists()).toBe(true);
+  });
+
+  test('Should set mode selection not mandatory', () => {
+    const wrapper = createWrapper();
+
+    const selector = wrapper.findComponent(Selector);
+    expect(selector.props()).toMatchObject({
+      mandatory: false,
+    });
+  });
+});
---
+++ src/front/priority-modificator/priority-modificator.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import { useFormStore } from '../form-store.ts';
+import Selector from '../shared/selector.vue';
+import CustomPriorityModificator from './custom-priority-modificator.vue';
+
+const store = useFormStore();
+</script>
+<template>
+  <Selector v-model:selectedMode="store.priorityModificatorMode" :mandatory="false">
+    <CustomPriorityModificator />
+  </Selector>
+</template>
